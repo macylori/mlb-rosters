e7a8c37bec95a3aa2135bfcc7459db28e886dda6
Macy Lori <lori.4@buckeyemail.osu.edu>
2022-05-30 22:05:27 -0400

    Added node_modules

---
+++ mlb-rosters/components/Teams.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+class Teams extends React.Component {
+  render() {
+    return(
+      <div>
+        <Jumbotron>
+          <h1>MLB Teams</h1>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default Teams;
