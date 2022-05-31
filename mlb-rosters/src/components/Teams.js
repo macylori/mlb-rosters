1c964ef121fc949b79f3405864e4b5da3dbddf5a
Macy Lori <lori.4@buckeyemail.osu.edu>
2022-05-31 10:31:21 -0400

    Added bootstrap

--- mlb-rosters/src/components/Teams.js
+++ mlb-rosters/src/components/Teams.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+
 import '../App.css';
 
-const Teams = () => {
-  return(
-    <h1>MLB Teams</h1>
-  );
+class Teams extends React.Component {
+  render () {
+    return(
+      <div>
+        <h1>MLB Teams</h1>
+        <Card>
+          <Card.Text>Milwaukee Brewers</Card.Text>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Teams;
