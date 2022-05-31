9ef1942fd23ce957a6628fdd9846303c50a37c1c
Macy Lori <lori.4@buckeyemail.osu.edu>
2022-05-31 10:37:52 -0400

    Created SpringBoot back-end component

---
+++ mlb-rosters-server/src/main/java/com/mlb/mlbrostersserver/MlbRostersServerApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.mlbrostersserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbRostersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbRostersServerApplication.class, args);
+	}
+
+}
---
+++ mlb-rosters-server/src/test/java/com/mlb/mlbrostersserver/MlbRostersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.mlbrostersserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbRostersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
