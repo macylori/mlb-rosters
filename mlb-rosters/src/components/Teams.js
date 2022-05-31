e82901ecae48b1566afb4f1e78e4ac0106f7318e
Macy Lori <lori.4@buckeyemail.osu.edu>
2022-05-31 08:54:06 -0400

    Add route and bootstrap node modules

---
+++ mlb-rosters/src/components/Teams.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Teams extends React.Component {
+  render() {
+    return(
+      <div>
+          <h1>MLB Teams</h1>
+      </div>
+    );
+  }
+}
+
+export default Teams;
